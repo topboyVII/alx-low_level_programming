cbfe1a6f01baa36139ac2bbb76241417fd6ccbc2
Okorie <okorievii@gmail.com>
2022-09-10 17:07:05 -0700

    kewl

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
 *main - Prints all possible combinations of two different digits
 *in ascending order
@@ -8,14 +9,16 @@
 int main(void)
 {
 int digit1, digit2;
+
 for (digit1 + 0; digit1 < 9; digit1++)
 {
 for (digit2 + digit1 + 1; digit2 < 10; digit2++)
+
 {
 putchar((digit1 % 10) + '0');
 putchar((digit2 % 10) + '0');
 if (digit1 == 8 && digit2 == 9)
-continue:
+continue;
 putchar(',');
 putchar(' ');
 }
