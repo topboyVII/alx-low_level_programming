b925bb2188d95cbdec5da83287fafb041545e857
Okorie <okorievii@gmail.com>
2022-09-15 05:12:22 -0700

    welp2

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -28,12 +28,12 @@ while (h1 <= a)
 m2 = 0;
 while (m2 <= 5)
 {
-m1 = 0;	
+m1 = 0;
 while (m1 <= 9)
 {
 _putchar('0' + h2);
 _putchar('0' + h1);
-_putchar(':');		
+_putchar(':');
 _putchar('0' + m2);
 _putchar('0' + m1);
 _putchar('\n');
