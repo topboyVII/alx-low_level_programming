d63a615077c713974de475d225269959f79fdb15
Okorie <okorievii@gmail.com>
2022-09-19 17:12:28 -0700

    please do

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+*
+* swap_int - swap two integers
+*
+* @a: pointer to a
+*
+* @b: pointer to b
+*
+*     
+*
+*
+* Return: nothing
+*/
+void swap_int(int *a, int *b)
+{
+int aux;
+aux = *a;
+*a = *b;
+*b = aux;
+}
